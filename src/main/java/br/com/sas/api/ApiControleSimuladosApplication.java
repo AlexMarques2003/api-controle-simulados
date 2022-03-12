65404bd39473f892c452ffe2b80c009e4de3c366
Alex Marques <alexmarques2003@gmail.com>
2022-03-12 14:16:21 -0300

    Atualização

---
+++ src/main/java/br/com/sas/api/ApiControleSimuladosApplication.java
@@ -0,0 +1,13 @@
+package br.com.sas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleSimuladosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleSimuladosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sas/api/ApiControleSimuladosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleSimuladosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
