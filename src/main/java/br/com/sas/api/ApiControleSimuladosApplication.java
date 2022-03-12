52ca07ae77d1da85fbcf3fe1bea3b10b79c9e43a
Alex Marques <alexmarques2003@gmail.com>
2022-03-12 15:07:38 -0300

    Atualização do travis java 11

--- src/main/java/br/com/sas/api/ApiControleSimuladosApplication.java
+++ src/main/java/br/com/sas/api/ApiControleSimuladosApplication.java
@@ -8,6 +8,7 @@ public class ApiControleSimuladosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiControleSimuladosApplication.class, args);
+		System.out.println("Aplicação no ar");
 	}
 
 }
